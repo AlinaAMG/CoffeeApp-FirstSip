24efa85239e11f0298cdbda1a4968bffee05c912
Ilyas_N <ilyas@nioyatech.com>
2025-05-03 22:23:08 +0200

    add validate in model files

--- routes/testimonial-routes.js
+++ routes/testimonial-routes.js
@@ -3,11 +3,9 @@ const verifyJWT = require('../auth/auth')
 
 const {getTestimonials, addTestimonial, deleteTestimonial, updateTestimonial} =require('../controllers/testimonials-controller')
 
-
 router.get('/api/testimonials', getTestimonials)
 router.post('/api/testimonials', addTestimonial)
 router.delete('/api/testimonail/delete',deleteTestimonial)
 router.put('/api/testimonail/update',updateTestimonial)
-
 
 module.exports=router
