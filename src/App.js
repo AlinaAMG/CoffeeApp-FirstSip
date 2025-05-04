e9a745f20f2f670f67d8a73824d1c5f78d550513
Alina <alina_gabur@yahoo.com>
2025-05-04 11:01:28 +0200

    404page added + extra features

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import ContactForm from './components/ContactForm/ContactForm';
 import CoffeeDetail from "./components/CoffeeDetails/CoffeeDetails";
 import FavoritesPage from './components/Favorites/Favorites';
 import Cart from "./components/CartPage/Cart";
+import CheckoutPage from './components/CheckoutPage/CheckoutPage';
+import NotFoundPage from './components/404Page/404';
 
 function App() {
   return (
@@ -29,13 +31,15 @@ function App() {
         <Route path="/shop/premium-blends" element={<AllCoffees />} />
         <Route path="/our-origins" element={<OurOrigins />} />
         <Route path="/shop/:id" element={<CoffeeDetail />} />
-        <Route path = "/favorites" element={<FavoritesPage/>}/>
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/check-out" element={<CheckoutPage/>}/>
         {/* <Route path="/coffee-box" element={<CoffeeBox />} /> */}
         {/* <Route path="/testimonials" element={<Testimonials />} /> */}
         <Route path="/contact" element={<ContactForm />} />
         <Route path="/about" element={<OurStory />} />
          <Route path="/cart" element={<Cart />}/> 
-         {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path = "*" element={<NotFoundPage/>}/>
       </Routes>
 
       <Footer />
